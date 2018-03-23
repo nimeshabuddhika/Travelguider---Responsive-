bed4a88cee2ce539700b2fd43bf88679848c4aa4
Nimesha Buddhika <135071J@uom.lk>
2018-03-24 01:10:57 +0530

    navbar added

---
+++ public/js/nav-js-3/custom-nav-scroll.js
@@ -0,0 +1,29 @@
+
+/**
+ * Make navbar active 
+ */
+
+$("body").waypoint(function() {
+    $(".navbar").toggleClass("navbar__initial");
+    alert("Hi");
+    return false;
+}, { offset: "-20px" });
+
+/**
+ * Smooth scroll to anchor
+ */
+
+// $(function() {
+//   $('a[href*=#]:not([href=#])').on('click',function() {
+//     if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+//       var target = $(this.hash);
+//       target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+//       if (target.length) {
+//         $('html,body').animate({
+//           scrollTop: target.offset().top
+//         }, 1000);
+//         return false;
+//       }
+//     }
+//   });
+// });
